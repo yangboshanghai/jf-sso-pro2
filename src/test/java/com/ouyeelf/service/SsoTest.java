a98b92e1b2bf41ca1ee053039abe7c2229a6171d
杨波 <yangbo197506@ouyeel.com>
2022-08-25 18:00:21 +0800

    新加代码

--- src/test/java/com/ouyeelf/service/SsoTest.java
+++ src/test/java/com/ouyeelf/service/SsoTest.java
@@ -15,11 +15,11 @@ public class SsoTest {
     String clientId="SSO_WEB";
     String clientSecret="a1cf82d4b6e566be8943c6a4084635d2";
     String publicKey="MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCXOkmfyF7Vl5CNc5IhFkoctdMuUVz9EnU9+1esKaAKWTmLNizqsjfqkf0YS2okkn1cUYO1cUf9zs3FhHs00Sp7tPLRit9QD1yFRuXujbMH80dUteUmiEPjDpB9bvIU6z0sN4pRBEnstoxhGnEDBgPaoDzRr/guM6beMfIGKD4aqQIDAQAB";
-    String service_token="O6Iy3tTMbCEtM4KJQ5FBs44Hm4uyWhAJC8W+zMwVyek0u3SIQuULmWOKsx6pvOb1vz0RBxfIHbL9MwAfLhWfVg==";
+    String service_token="O6Iy3tTMbCEtM4KJQ5FBs29CIHZ16dreJFpuM7XpMqlPT2Q8zmkUIVALutmqXlaTvz0RBxfIHbL9MwAfLhWfVg==";
     //内部
     String ssoToken="JFoNZj+mPBzMme/amZo4Ox1qlk+DTTuCseP+zxPZ1NW85+CV5Rc05X7I5kBjZPwn";
     //外部
-    String ssoToken_w="d01a2937cc4842d2807e15df627a8bdb";
+    String ssoToken_w="3b9d5306976744758accd58ffedd91ad";
     @Test
     public void getServiceToken() throws Exception {
         String orderId=System.currentTimeMillis()+"";
@@ -34,7 +34,9 @@ public class SsoTest {
         infParam.put("param",param);
         String body=JSON.toJSONString(infParam);
         url2=url2+"getServiceToken";//接口地址
-        String outstr= HttpUtil.post(url2,body);
+        HashMap<String, String> header = new HashMap<>();//存放请求头，可以存放多个请求头
+        header.put("Content-Type", "application/json");
+        String outstr= HttpRequest.post(url2).addHeaders(header).body(body).execute().body();
         System.out.println(outstr);
 //        {"isSuccess":true,"status":"0","message":"请求成功","orderId":"1661226957565","data":"O6Iy3tTMbCEtM4KJQ5FBs44Hm4uyWhAJC8W+zMwVyek0u3SIQuULmWOKsx6pvOb1vz0RBxfIHbL9MwAfLhWfVg=="}
     }
@@ -53,6 +55,7 @@ public class SsoTest {
         String body= JSON.toJSONString(infParam);
         Map header=new HashMap();//将服务端TOKEN放在http header中
         header.put("service_token",service_token);
+        header.put("Content-Type", "application/json");
         System.out.println(url);
         String outstr= HttpRequest.post(url).addHeaders(header).body(body).execute().body();
         System.out.println(outstr);
@@ -67,6 +70,7 @@ public class SsoTest {
         Map header=new HashMap();//将服务端TOKEN放在http header中
         header.put("service_token",service_token);
         header.put("ssoToken",ssoToken);
+        header.put("Content-Type", "application/json");
         url="http://10.60.36.245:8080/sso/messageCode/getVeriCode";
         System.out.println(url);
         String outstr= HttpRequest.get(url).addHeaders(header).body(body).execute().body();
@@ -87,6 +91,7 @@ public class SsoTest {
         Map header=new HashMap();//将服务端TOKEN放在http header中
         header.put("service_token",service_token);
         header.put("ssoToken",ssoToken);
+        header.put("Content-Type", "application/json");
         String outstr= HttpRequest.post(url).addHeaders(header).body(body).execute().body();
         System.out.println(outstr);
     }
@@ -98,6 +103,7 @@ public class SsoTest {
         Map header=new HashMap();//将服务端TOKEN放在http header中
         header.put("service_token",service_token);
         header.put("ssoToken",ssoToken);
+        header.put("Content-Type", "application/json");
         Map infParam=new HashMap();//接口参数
         infParam.put("smsCode","666666");
         String body= JSON.toJSONString(infParam);
@@ -112,6 +118,7 @@ public class SsoTest {
         Map header=new HashMap();//将服务端TOKEN放在http header中
         header.put("service_token",service_token);
         header.put("ssoToken",ssoToken);
+        header.put("Content-Type", "application/json");
         Map infParam=new HashMap();//接口参数
         infParam.put("userId","shanghairunyi");
         infParam.put("password","Tb12345678");
